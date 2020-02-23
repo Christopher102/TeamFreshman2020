46c42b7f0a20b7914cdbc20a7d949471ae832a19
Christopher102 <Christopher.Fioti1@marist.edu>
2020-02-23 13:21:56 -0500

    hello

--- app/views.py
+++ app/views.py
@@ -38,8 +38,8 @@ def fetch_posts():
 def index():
     fetch_posts()
     return render_template('index.html',
-                           title='YourNet: Decentralized '
-                                 'content sharing',
+                           title='ChainFacts: Real Facts from The People '
+                                 '',
                            posts=posts,
                            node_address=CONNECTED_NODE_ADDRESS,
                            readable_time=timestamp_to_string)
